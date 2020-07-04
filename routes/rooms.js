786978f78c808ce76e1fc98aa81e84822697535d
Amir Barkam <amirbarkam6@gmail.com>
2020-07-04 13:55:59 -0700

    Switch player model to room model

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Room = require('../models/Player');
+const Room = require('../models/Room');
 
 router.get('/', async (req, res) => {
   try {
